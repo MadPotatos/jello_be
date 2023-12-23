b5aa01aa4552e66f3885033fa9e1063392e36557
viet <viet7896@gmail.com>
2023-12-23 23:23:45 +0700

    FEATURE: update profile info

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,8 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { CreateUserDto } from './dto/dto/user.dto';
+import { CreateUserDto, UpdateUserDto } from './dto/dto/user.dto';
 import { hash } from 'bcrypt';
+import { UserProfile } from './entities/user-profile.entity';
 
 @Injectable()
 export class UserService {
@@ -34,11 +35,43 @@ export class UserService {
       },
     });
   }
-  async findById(id: number) {
-    return await this.prisma.user.findUnique({
+  async findProfile(id: number): Promise<UserProfile> {
+    const user = await this.prisma.user.findUnique({
       where: {
         id: id,
       },
     });
+    if (!user) throw new ConflictException('user not found');
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+    };
+  }
+
+  async updateInfo(id: number, body: UpdateUserDto): Promise<UserProfile> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+    if (user) throw new ConflictException('email duplicated');
+    const updatedUserRaw = await this.prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        ...body,
+      },
+    });
+
+    return {
+      id: updatedUserRaw.id,
+      name: updatedUserRaw.name,
+      email: updatedUserRaw.email,
+      avatar: updatedUserRaw.avatar,
+    };
   }
 }
